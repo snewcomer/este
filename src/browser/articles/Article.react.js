bb91c1db2dcfbbae85ec0af3524521b0e73e6e57
snewcomer <snewcomer24@gmail.com>
2016-06-11 22:53:01 -0700

    [CSS]: article w/ comment bubble and hearts

--- src/browser/articles/Article.react.js
+++ src/browser/articles/Article.react.js
@@ -1,4 +1,5 @@
 import './Article.scss';
+import { Link } from 'react-router';
 import Component from 'react-pure-render/component';
 import React, { PropTypes } from 'react';
 // import { FormattedMessage, FormattedHTMLMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
@@ -18,6 +19,14 @@ export default class Article extends Component {
         <section className="article-body">
           <p>{article.body}</p>
         </section>
+        <div className="flex-container">
+          <button className="likes">&hearts; {article.likes}</button>
+          <div className="comment-wrapper">
+            <Link className="button" to={`/`}>
+              <span className="comment-bubble"></span>
+            </Link>
+          </div>
+        </div>
       </div>
     )
   }
